33027a8f4129d7994796739fcb223c1fda803dbc
AllusiveBox <42219971+AllusiveBox@users.noreply.github.com>
2019-01-26 12:22:15 -0500

    Initial Code Commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿/**
+ * 
+ * Dice Bot DnD Bot Script
+ * Version 1.0.0
+ * Author: AllusiveBox
+ * Date Started: 01/26/19
+ * Last Updated: 01/26/19
+ * Last Updated By: AllusiveBox
+ * 
+ */
+
+process.chdir(__dirname); // Ensure Working Directory is Same as Current File
+
+// Required Libraries and Files
+const Disord = require('discord.js');
+const botToken = require('./assets/files/token.json');
+const config = require('./assets/files/config.json');
